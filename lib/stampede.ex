14a586e8811cc40ed2a0f3ca7ffaf592c56f23aa
Michał Śledź <michal.sledz@swmansion.com>
2021-11-18 19:46:14 +0100

    Add delay option

--- lib/stampede.ex
+++ lib/stampede.ex
@@ -1,15 +1,31 @@
 defmodule Stampede do
+  @typedoc """
+  Stampede runtime options
+
+  * count - number of mustangs that will be spawned
+  * delay - delay between subsequent spawns
+  """
+  @type options_t() :: %{
+          count: pos_integer(),
+          delay: :timer.time()
+        }
+
   def start({mustang_module, mustang_options}) do
     launch_browser() |> mustang_module.run(mustang_options)
   end
 
   def start({mustang_module, mustang_options}, options) do
     browser = launch_browser(options)
+    IO.inspect(options, label: :opts)
 
     if options.count do
+      delay = Map.get(options, :delay, 0)
+
       tasks =
         Enum.map(1..options.count, fn _x ->
-          Task.async(fn -> mustang_module.run(browser, mustang_options) end)
+          task_ref = Task.async(fn -> mustang_module.run(browser, mustang_options) end)
+          :timer.sleep(delay)
+          task_ref
         end)
 
       Task.await_many(tasks, :infinity)
